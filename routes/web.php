1220f208f335e07faec0341af733da9b34893416
MyProgramings <reader1999a@gmail.com>
2025-05-27 12:30:48 +0300

    done add header

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
